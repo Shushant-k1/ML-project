3ad0e8563a44beea724a3f878094c475b446eb5f
shushant kumar <104075647+everythingcanbedone@users.noreply.github.com>
2022-08-10 01:28:24 +0530

    Update shuffle_the_desk_of_cards.py

--- shuffle_the_desk_of_cards.py
+++ shuffle_the_desk_of_cards.py
@@ -1,3 +1,8 @@
+# we want to shuffle an array of 52 cards from 0 to 1 with no repetations.
+#first fill the array with the values in order , then swap the value with random value in range
+#It"s possible that an element will be swap with itself ,but there is no problem with that
+
+
 import random
 def shuffle(cards):
     max=len(cards)-1
